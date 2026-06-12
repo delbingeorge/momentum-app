b66051986f03e1cc98e33f9d3b21c1dfed5e47b3
Delbin George <64653923+delbingeorge@users.noreply.github.com>
2026-06-12 12:44:27 +0530

    chore: rename sheet conventions

--- features/profile/index.ts
+++ features/profile/index.ts
@@ -1,4 +1,4 @@
 export { ProfilePanel } from "./components/profile-panel";
-export { ExportSheet } from "./components/export.sheet";
-export { ReminderSheet } from "./components/reminder.sheet";
+export { ExportSheet } from "./components/export-sheet";
+export { ReminderSheet } from "./components/reminder-sheet";
 export { scheduleReminder } from "./lib/reminder-scheduler";
